f2fac3f17646081b56ebedd357d734dcba46614f
Anurag Pandey <anurag2992000@gmail.com>
2024-07-28 18:43:32 +0530

    Slight CSS modifications

--- src/components/Login.js
+++ src/components/Login.js
@@ -106,6 +106,7 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
+        className="h-screen object-cover w-screen"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/21a8ba09-4a61-44f8-8e2e-70e949c00c6f/6678e2ea-85e8-4db2-b440-c36547313109/IN-en-20240722-POP_SIGNUP_TWO_WEEKS-perspective_WEB_3457a8b1-284d-4bb5-979e-2a2e9bb342b3_large.jpg"
           alt="bg-logo"
         />
